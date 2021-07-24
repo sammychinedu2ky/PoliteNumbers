f870b19ee6260c747594fff2b2ae74df6283a20e
Samson Amaugo <sammychinedu2ky@gmail.com>
2021-07-24 08:59:56 +0100

    formatted code

--- Program.cs
+++ Program.cs
@@ -9,30 +9,31 @@ namespace swacblooms
         {
             Console.WriteLine("Please input your target value: ");
             int input = Convert.ToInt32(Console.ReadLine());
-
             Console.WriteLine(ConsecutiveGroups(input));
-           
+
         }
-        static int ConsecutiveGroups(int target){
+        static int ConsecutiveGroups(int target)
+        {
             int product = 2 * target;
             int count = 0;
             int n = 2;
-            List<List<int>> ranges = new List<List<int>>{};
+            List<List<int>> ranges = new List<List<int>> { };
 
-            while(true){
-                double numerator = product - n*n + n;
+            while (true)
+            {
+                double numerator = product - n * n + n;
                 double denominator = 2 * n;
-                double a = numerator/denominator;
-                if(a <= 0) break;
-                if((int)a == a){
-                    List<int> range = Enumerable.Range((int)a,n).ToList();
+                double a = numerator / denominator;
+                if (a <= 0) break;
+                if ((int)a == a)
+                {
+                    List<int> range = Enumerable.Range((int)a, n).ToList();
                     ranges.Add(range);
                     count++;
-                   
                 }
-                 n++;
+                n++;
             }
-            ranges.ForEach(i=>Console.WriteLine($"{string.Join('+',i)} = {target}"));
+            ranges.ForEach(i => Console.WriteLine($"{string.Join('+', i)} = {target}"));
             return count;
         }
     }
